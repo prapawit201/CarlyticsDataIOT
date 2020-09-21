81e60d56d238eccba717d9d7aae2e36095a95de6
zBabEs <piyapat43@gmail.com>
2020-09-21 11:36:23 +0700

    [Edit] Api fetch data

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ MongoClient.connect(
           if (err) return res.status(500).send(err.toString());
           axios
             .post(
-              "https://gentle-dawn-55414.herokuapp.com/fetchData",
+              "https://carlytic-data-detect.herokuapp.com/fetchData",
               ...result.ops
             )
             .then((result) => {
